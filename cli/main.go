1a9009cc791f07dcd40102796027bd9f3e29cb61
Scott Lester <dev@meantimecyber.com>
2026-07-31 20:31:30 +0100

    update connect logic to handle Wi-Fi and password

--- cli/main.go
+++ cli/main.go
@@ -5,16 +5,20 @@ import (
 	"flag"
 	"fmt"
 
-	"github.com/MeanTimeCyber/go-busy-bar/client"
+	barclient "github.com/MeanTimeCyber/go-busy-bar/client"
 )
+
+const defaultUSBAddress = "10.0.4.20"
 
 func main() {
 	// define and process args
-	var command, ipAddress string
+	var command, ipAddress, password string
 	flag.StringVar(&command, "c", "status", "Command for the busy bar")
-	flag.StringVar(&ipAddress, "ip", "10.0.4.20", "IP address of the busy bar - assumes it's over USB on the fixed address")
+	flag.StringVar(&ipAddress, "ip", defaultUSBAddress, "IP address of the busy bar - assumes it's over USB on the fixed address")
+	flag.StringVar(&password, "p", "", "Password for the busy bar - required if on a wifi address")
 	flag.Parse()
 
+	// Check if a command was provided
 	if command == "" {
 		println("No command specified")
 		flag.Usage()
@@ -22,16 +26,23 @@ func main() {
 		return
 	}
 
-	// TODO add password, check its present if on a wifi address
+	// Create a new client with the specified endpoint and password
+	client, err := getClient(ipAddress, password)
+	if err != nil {
+		fmt.Printf("Error creating client: %v\n", err)
+		return
+	}
 
-	// Create a new client with the specified endpoint
+	// Create a context for the API calls
 	ctx := context.Background()
-	c := client.NewClient("http://" + ipAddress + "/")
 
+	// TODO get network status to check we're connected to the device, and if not, print a message and exit
+
+	// Execute the provided command
 	switch command {
 	// get the device status information and print it to the console
 	case "status":
-		status, err := c.GetStatus(ctx, nil)
+		status, err := client.GetStatus(ctx, nil)
 		if err != nil {
 			fmt.Printf("Error getting device status: %v\n", err)
 			return
@@ -39,33 +50,67 @@ func main() {
 		status.PrettyPrint()
 	// get the device name, info and account details and print them to the console
 	case "info":
-		deviceName, err := c.GetName(ctx, nil)
+		deviceName, err := client.GetName(ctx, nil)
 		if err != nil {
 			fmt.Printf("Error getting device name: %v\n", err)
 			return
 		}
 		deviceName.PrettyPrint()
 
-		deviceInfo, err := c.GetStatusDevice(ctx, nil)
+		deviceInfo, err := client.GetStatusDevice(ctx, nil)
 		if err != nil {
 			fmt.Printf("Error getting device info: %v\n", err)
 			return
 		}
 		deviceInfo.PrettyPrint()
 
-		accountInfo, err := c.GetAccountInfo(ctx, nil)
+		accountInfo, err := client.GetAccountInfo(ctx, nil)
 		if err != nil {
 			fmt.Printf("Error getting account info: %v\n", err)
 			return
 		}
 		accountInfo.PrettyPrint()
+	// get the API version and print it to the console
+	case "version":
+		apiVersion, err := client.GetVersion(ctx, nil)
+		if err != nil {
+			fmt.Printf("Error getting API version: %v\n", err)
+			return
+		}
+		apiVersion.PrettyPrint()
 	default:
 		println("Unknown command:", command)
 		printCommands()
 	}
 }
 
+// getClient creates a new Busy Bar client with the specified IP address and password.
+func getClient(ipAddress, password string) (*barclient.Client, error) {
+	var client *barclient.Client
+
+	// Check we have a password if on a Wi-Fi address
+	if ipAddress != defaultUSBAddress {
+		// If the IP address is not the default USB address, we assume it's a Wi-Fi address and require a password
+		if password == "" {
+			flag.Usage()
+			return nil, fmt.Errorf("password is required for access over Wi-Fi address")
+		}
+
+		// Create a new client with the specified endpoint and password
+		client = barclient.NewClientWithAPIKey("http://" + ipAddress +"/", password)
+
+	} else {
+		// Create a new client with the specified endpoint
+		client = barclient.NewClient("http://" + ipAddress + "/")
+	}
+
+	return client, nil
+}
+
+// printCommands prints the available commands for the busy bar.
 func printCommands() {
 	println("Available commands:")
 	println("  status - Get the status of the busy bar")
+	println("  info - Get the device info of the busy bar")
+	println("  version - Get the API version of the busy bar")
 }
